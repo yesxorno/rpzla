efc4f1ff686546a764bb29ca1ec2c98203c7b98d
Yes Xor No <yesxorno@gmail.com>
2012-10-15 10:21:44 +0200

    BIND log config and parsing changes

--- bin/rpzla-bind-filter.pl
+++ bin/rpzla-bind-filter.pl
@@ -8,14 +8,14 @@ use File::Basename;
 use English;
 
 # command-line options
-use constant OPT_FILE		=> 'file';
-use constant OPT_HELP           => 'help';
-use constant OPT_MANUAL         => 'manual';
-use constant OPT_VERSION        => 'Version';
+use constant OPT_FILE	=> 'file';
+use constant OPT_HELP   => 'help';
+use constant OPT_MANUAL => 'manual';
+use constant OPT_VERSION => 'Version';
 
-# cut out the fields we need from the BIND query log (input filter)
-use constant FILTER		=> " grep ':rpz QNAME CNAME rewrite ' ";
-use constant OUTPUT_SEP		=> ' ';
+# filtering now down internally (ignore FILTER)
+# use constant FILTER	=> " grep --line-buffered '[ ]rewrite[ ]' ";
+use constant OUTPUT_SEP	=> ' ';
 
 my $version = '0.1';
 my $ME = basename($PROGRAM_NAME);
@@ -56,7 +56,13 @@ sub parse_command_line()
         {
                 pod2usage(2);
         }
-	elsif ( ! -f $options{OPT_FILE()} or ! -r $options{OPT_FILE()} )
+	elsif 
+	( 
+		(! -f $options{OPT_FILE()} or ! -r $options{OPT_FILE()})
+	and
+		# useful for testing; use stdin
+		'-' ne $options{OPT_FILE()}
+	)
 	{
 		err("Log file argument '" . $options{OPT_FILE()} . 
 			"' does not exist or cant be read.");
@@ -66,33 +72,71 @@ sub parse_command_line()
 }
 
 # given a line from the named log, reformat to:
-# date, time, client ip, query domain, rpz zone
+# date, time, client ip address, query domain, rpz zone
+#
+# Exmaple input:
+# 
+# 15-Oct-2012 19:24:59.740 client ::1#52394 (nastynasty.com): rpz QNAME CNAME rewrite nastynasty.com via nastynasty.com.local.rpz
+#
+# Example output:
+#
+# 15-Oct-2012 19:24:59.740 ::1 nastynasty.com local.rpz
+#
+# ie.
+#
+# date time 'ipv6 loopback address' 'query domain' 'rpz zone name'
 sub reformat($)
 {
-	my @w = split(' ', shift);
-	# unclear; needs improvement: too much 'magic'
-	my ($date, $time, $ip_port, $response, $query_zone) = 
-		($w[0], $w[1], $w[5], $w[10], $w[12]);
-	my ($hms, $fractional_seconds) = split('[.]', $time);
-	my ($ip, $rest) = split('[#]', $ip_port);
-	# Wrong way to do this: strip the query domain, rather than 
-	# relying on the fact that the rpz zone name starts with rpz (idiot)
-	my @domains = split('[.]', $query_zone);
-	my @rpz_zone = ( );
-	my $current = '';
-	# FIX ME: dont rely on rpz as the first part of the rpz zone name
-	while ( 'rpz' ne $current and scalar(@domains) )
+	my $line = shift;
+	my $parsed = 0;
+	# Format is:
+	#
+	# (date) (time) client (ip) \((query)\): rpz 'word' 'word' rewrite
+	# (query) via (rpz-zone)
+	#
+	# where things in parens are matched for extration.
+	#
+	my $format = '([\w\-\:\.]+) ([\w\:\.]+) client ([\w\:\.\#]+) [(]([\w\.]+)[)]: rpz [\w]+ [\w]+ rewrite ([\w\.]+) via ([\w\.]+)';
+	if ( $line !~ m/$format/ )
 	{
-		$current = pop(@domains);
-		unshift(@rpz_zone, $current);
+		err("Unexpected format from BIND log " . $options{OPT_FILE()});
+		err("Line was: $line");
+		# abort
+		return undef;
 	}
-	# what is left is the query
-	my @query = @domains;
-	return join
-	(
-		OUTPUT_SEP(), $date, $hms, $ip, 
-		join('.', @query), 
-		join('.', @rpz_zone)
+	my ($date, $time, $ip_full, $query1, $query2, $zone_full) = 
+		($1, $2, $3, $4, $5, $6);
+	my $rpz_zone = undef;
+	# Split out core details from entries combined with more data
+	# Ignore fractional seconds
+	my ($hms, $fractional_seconds) = split('[.]', $time);
+	# Ignore client port number
+	my ($ip, $port) = split('[#]', $ip_full);
+	# Check queries match
+	if ( $query1 ne $query2 )
+	{
+		err("Unexpected: name of query domain differs.  Bad parsing.");
+		err("Query 1: '$query1' != Query 2: '$query2'");
+	}
+	# strip the query from the font of the rpz zone name
+	elsif ( $zone_full !~ m/$query1[.]([\w\.]+)/ )
+	{
+		err("Unexpected: query domain not a part of the rpz zone name");
+		err("Query domain: '$query1', Zone name (full): '$zone_full'");
+	}
+	else
+	{
+		$parsed = 1;
+		$rpz_zone = $1;
+	}
+	# join and return
+	return 
+	( 
+		$parsed 
+	? 
+		join(OUTPUT_SEP(), $date, $hms, $ip, $query1, $rpz_zone)
+	:
+		undef
 	);
 }
 
@@ -100,10 +144,11 @@ sub reformat($)
 # that the 'to db' logger wants.
 sub main()
 {
-	local $| = 0;
+	local $| = 1;
 	my $retval = 1;
 	my $file = $options{OPT_FILE()};
-	my $cmd = " tail -f $file | " . FILTER() . " | ";
+	# my $cmd = " tail -f $file | " . FILTER() . " | ";
+	my $cmd = " tail -f $file | ";
 	if ( ! open(STREAM, $cmd) )
 	{
 		err("Failure attempting to watch '$file'.  Aborting.");
@@ -111,10 +156,12 @@ sub main()
 	else
 	{
 		# msg("Running '$cmd'");
+		my $data = undef;
 		while ( <STREAM> )
 		{
 			chomp($_);
-			print reformat($_) . "\n";
+			$data = reformat($_) if ( m/ rewrite / );
+			print "$data\n" if defined($data);
 		}
 		msg("Ending");
 	}
@@ -131,40 +178,31 @@ __END__
 
 =head1 NAME
 
-bind-query-log-filter-rpz.pl - parse a BIND query log extracting RPZ responses
+rpzla-bind-filter.pl - parse a BIND query log extracting RPZ responses
 
 =head1 SYNOPSIS
 
-        bind-query-log-filter-rpz.pl <-h|-m|-V>
+        rpzla-bind-filter.pl <-h|-m|-V>
 or
-        bind-query-log-filter-rpz.pl <-f path-to-file>
+        rpzla-bind-filter.pl <-f path-to-file>
 
 =head1 DESCRIPTION
 
-bind-query-log-filter-rpz.pl watches (ie. tail(1)) a log containing
-severity info data for queries to a BIND server utilising Response
-Policy Zones.
-
-It is required that all RPZ zones start with 'rpz.'.  This is used
-in parsing.
+rpzla-bind-filter.pl watches (ie. tail(1)) a log containing
+data for queries to a BIND server utilising Response
+Policy Zone (RPZ) that have used the RPZ facility.
 
 For each line that matches a response policy being used the following
 data is printed to stdout:
 
   Date (28-Mar-2012)
   Time (17:34:59, not that we strip the fractional seconds)
-  IP address of querier (IPv4 or IPv6, whatever the log records)
+  IP address of the client (IPv4 or IPv6, whatever the log records)
   Query (i.e www.verynasty.com)
   Name of rpz zone which responded (e.g rpz.local)
 
-Could give 
-
-  Response (Whatever the DNS gave.  E.g an IP or a CNAME)
-
-Should we ??  Makes no sense for a constant walled garden.
-Perhaps useful otherwise.
-
-The output is all on one line and single whitspace separated.
+The output is all on one line and single whitspace separated, which
+is expected by the rpzla-bind-to-db.pl script.
 
 =head1 OPTIONS
 
@@ -172,7 +210,7 @@ The output is all on one line and single whitspace separated.
 
 =item B<-f, --file path-to-file>
 
-The file must be a bind log of query information of severity info.
+The file must be a bind log containing RPZ log data.
 
 =item B<-h, -?, --help>
 
@@ -190,8 +228,7 @@ Print the software version.
 
 =head1 SEE ALSO
 
-rpz-log-apache-filter.pl(1), rpz-log-apache-to-db.pl(1), 
-rpz-log-bind-to-db.pl(1)
+rpzla-bind-to-db.pl(1)
 
 =head1 AUTHOR
 
